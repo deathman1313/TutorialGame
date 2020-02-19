27411d09359ffaf483d4fab9d01e5fab4e40faf5
deathman1313 <55701442+deathman1313@users.noreply.github.com>
2020-02-19 19:40:58 +0000

    Add files via upload

--- js/scenes.js
+++ js/scenes.js
@@ -1,5 +1,8 @@
 class BaseScene extends Phaser.Scene {
     map;
+    player;
+    cursors;
+    camera;
     exit;
 
     constructor(key) {
@@ -12,8 +15,38 @@ class BaseScene extends Phaser.Scene {
         this.map.props = this.map.addTilesetImage('spritesheet_tiles', 'props-image');
         this.setCamera();
         this.map.background = this.add.image(this.camera.centerX, this.camera.centerY, 'background-image');
+        this.map.background.setScrollFactor(0);
         this.map.background.setScale(this.camera.height / this.map.background.height);
         this.createLayers();
+        this.camera.startFollow(this.player);
+        this.cursors = this.input.keyboard.createCursorKeys();
+        this.anims.create({
+            key: 'move',
+            frames: this.anims.generateFrameNumbers('player', {
+                start: 9,
+                end: 10
+            }),
+            frameRate: 10,
+            repeat: -1
+        });
+    }
+
+    update() {
+        if (this.cursors.left.isDown) {
+            this.player.setVelocityX(-500);
+            this.player.anims.play('move', true);
+            this.player.flipX = true;
+        } else if (this.cursors.right.isDown) {
+            this.player.setVelocityX(500);
+            this.player.anims.play('move', true);
+            this.player.flipX = false;
+        } else {
+            this.player.setVelocityX(0);
+            this.player.setFrame(3);
+        }
+        if (Phaser.Input.Keyboard.JustDown(this.cursors.space)) {
+            this.player.setVelocityY(-900);
+        }
     }
 
     setCamera() {
@@ -25,8 +58,21 @@ class BaseScene extends Phaser.Scene {
         this.map.createStaticLayer('background', [this.map.landscape, this.map.props], 0, 0);
         this.map.createStaticLayer('platforms', [this.map.landscape, this.map.props], 0, 0);
         this.exit = this.map.createDynamicLayer('exit', [this.map.landscape, this.map.props], 0, 0);
+        this.map.getObjectLayer('objects').objects.forEach(function(object){
+            if (object.type === 'playerSpawn') {
+                this.createPlayer(object.x, object.y);
+            }
+        }, this);        
         this.map.createStaticLayer('foreground', [this.map.landscape, this.map.props], 0, 0);
     }
+
+    createPlayer(x, y) {
+        this.player = this.physics.add.sprite(x, y, 'player', 3);
+        this.player.setCollideWorldBounds(true);
+        this.collisionLayer = this.map.getLayer('platforms').tilemapLayer;
+        this.collisionLayer.setCollisionBetween(0, this.map.getLayer('platforms').tilemapLayer.tilesTotal);
+        this.physics.add.collider(this.player, this.collisionLayer);
+    }    
 }
 
 class Scene1 extends BaseScene {
@@ -39,6 +85,10 @@ class Scene1 extends BaseScene {
         this.load.image('props-image', 'assets/spritesheet_tiles.png');
         this.load.image('background-image', 'assets/blue_grass.png');
         this.load.tilemapTiledJSON('level1', 'assets/level1.json');
+        this.load.spritesheet('player', 'assets/alienYellow.png', {
+            frameWidth: 128,
+            frameHeight: 143
+        });        
     }
 
     create() {
@@ -59,6 +109,10 @@ class Scene2 extends BaseScene {
         this.load.image('props-image', 'assets/spritesheet_tiles.png');
         this.load.image('background-image', 'assets/blue_grass.png');
         this.load.tilemapTiledJSON('level1', 'assets/level2.json');
+        this.load.spritesheet('player', 'assets/alienYellow.png', {
+            frameWidth: 128,
+            frameHeight: 143
+        });        
     }
 
     create() {
